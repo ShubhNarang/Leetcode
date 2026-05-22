27bd4b7aadd0202dec7e6ea0d0ba1386f1b4c909
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-05-22 17:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (29.05%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> v;
-        for(int i = 0 ; i < nums1.size() ; i++){
-            for(int j = 0 ; j < nums2.size() ; j++ ){
-                if(nums1[i] == nums2[j]){
-                    v.push_back(nums1[i]);
-                    nums2[j]=INT_MIN;
-                    break;
-                }
-            }
+    unordered_map<int,int> m;
+    vector<int> v;
+    for(auto i:nums1){
+        m[i]++;
+    }
+    for(auto i:nums2){
+        if(m[i] >0){
+            v.push_back(i);
+            m[i]=0;
         }
-        sort(v.begin(), v.end());
-        auto it = unique(v.begin(), v.end());
-        v.erase(it, v.end());
-
+    }
         return v;
 
 
