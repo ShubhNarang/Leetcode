ab90bea98d95811596ab4df1ffd1ce6ffa14cd6b
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-06-12 11:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (61.60%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -10,7 +10,23 @@ public:
             }
 
         }
-        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+            if(nums[i]==0){
+                i++;
+            }
+            else if(nums[j]==1){
+                j--;
+            }
+            else{
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+
+        }
+
         return nums;
 
         
