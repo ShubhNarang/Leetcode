a860fcba458d3a7e972e0d2e2d1cb23807faa8e7
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-06-15 14:03:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.39%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-
-        int a = nums.size();
         int start = 0;
-        int end = a - 1;
-        int mid = start + (end - start)/2;
-        vector<int> v;
-        if(a == 0){
-            v.push_back(-1);
-            v.push_back(-1);
-            return v;
+        int end = nums.size()-1;
+        int mid = start +(end-start)/2;
+        int first = -1;
+        int last = -1;
+        vector<int> vec;
+
+        if(nums.size() == 0){
+            return {-1,-1};
         }
-        while (start <= end){
 
-            if(nums[mid] == target){
-                int temp = mid;
-                int temp1 = mid;
-                while(temp < nums.size() && nums[temp] == target){
-                    temp++;
-                }
-                while(temp1 >= 0 && nums[temp1] == target){
-                    temp1--;
-                }
-                v.push_back(temp1+1);
-                v.push_back(temp-1);
 
-                return v;
+        while(start<=end){
+            if(nums[mid]== target){
+                first = mid;
+                end = mid-1;    
             }
-            else if(nums[mid] > target ){
-                end = mid - 1;
+            else if(nums[mid]> target){
+                end = mid-1;
             }
-            else if(nums[mid] < target ){
-                start = mid + 1;
+            else if(nums[mid]< target){
+                start = mid+1;
             }
-
-            mid = start + (end - start)/2;
+            mid = start +(end-start)/2;
 
         }
-        v.push_back(-1);
-        v.push_back(-1);
-        return v;
-        
+        start = 0;
+        end = nums.size()-1;
+        mid = start +(end-start)/2;
+
+        while(start<=end){
+            if(nums[mid]== target){
+                last = mid;
+                start = mid+1;    
+            }
+            else if(nums[mid]> target){
+                end = mid-1;
+            }
+            else if(nums[mid]< target){
+                start = mid+1;
+            }
+            mid = start + (end-start)/2;
+
+        }
+        vec.push_back(first);
+        vec.push_back(last);
+        return vec;
     }
 };
