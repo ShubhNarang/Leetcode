4d93a3c7a149152ee9709bc0e9dcab7ef7e8ff5f
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-07-10 16:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (42.37%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,25 +4,18 @@ public:
         int i = m-1;
         int j = n-1;
         for(int k = nums1.size()-1 ; k>=0 ; k--){
-            if(i<0){
-                nums1[k]=nums2[j];
-                j--;
+            if(j<0){
+                break;
             }
-            else if(j > -1 && i > -1 && nums1[i]>nums2[j]){
+            if(i > -1 && nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
             }
-            else if(j > -1 && i > -1 && nums1[i]<nums2[j]){
+            else{
                 nums1[k]=nums2[j];
                 j--;
             }
-            else if(j > -1 && i > -1 && nums1[i]==nums2[j]){
-                nums1[k]=nums2[j];
-                nums1[k-1]=nums2[j];
-                i--;
-                j--;
-                k--;
-            }
+            
         }
          
     }
