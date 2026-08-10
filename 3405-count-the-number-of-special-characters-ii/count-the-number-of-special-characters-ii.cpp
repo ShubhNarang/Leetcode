65850616ea0b5708b480f5415be1bfa1538bb6c9
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-08-10 14:01:13 +0530

    Time: 86 ms (28.02%) | Memory: 24.3 MB (82.88%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -7,12 +7,15 @@ public:
             if(isupper(word[i]) ){
                 v[(word[i]-65)*2+1] = 1;
             }
-            if(islower(word[i]) && v[(word[i]-97)*2+1] == 0){
-                v[(word[i]-97)*2] = 1;
+            if(islower(word[i])){
+                if(v[(word[i]-97)*2+1] == 0){
+                    v[(word[i]-97)*2] = 1;
+                }
+                else{
+                    v[(word[i]-97)*2] = 0;
+                }
             }
-            if(islower(word[i]) && v[(word[i]-97)*2+1] == 1){
-                v[(word[i]-97)*2] = 0;
-            }
+            
         }
         for(int i = 0 ; i < v.size() ; i+=2){
             if((v[i] & v[i+1]) == 1){
