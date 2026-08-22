65bfcc23441a3185ea16d12de5a283d304d23569
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-08-23 01:25:28 +0530

    Time: 1 ms (19.82%) | Memory: 9.1 MB (96.09%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int> mp;
-        int a = 1;
-        for(auto i:s){
-            mp[i]++;
-        }
-        for(int i = 0 ; i<t.size() ; i++){
-            if(mp[t[i]] > 0){
-                mp[t[i]]--;
-            }
-            else{
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i = 0 ; i < t.size() ; i++){
+            if(s[i]!=t[i]){
                 return t[i];
             }
-
         }
         return 'a';
-        
     }
 };
