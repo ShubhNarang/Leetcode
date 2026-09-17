62a0c14b2ffd9215b1375a317cff047eff56d6cb
Shubh Narang <85398953+ShubhNarang@users.noreply.github.com>
2026-09-18 02:33:15 +0530

    Time: 40 ms (89.42%) | Memory: 29.1 MB (73.01%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -7,6 +7,8 @@ public:
             int start = i + 1;
             int end = nums.size()-1;
             int sum = nums[i];
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             while( start < end){
                 if(nums[start]+nums[end]+sum > 0){
                     end--;
@@ -16,12 +18,19 @@ public:
                 }
                 else if(nums[start]+nums[end]+sum == 0){
                     a.push_back({sum,nums[start],nums[end]});
-                    start = start + 1;
+                    start++;
+                    end--;
+
+                    while (start < end && nums[start] == nums[start - 1]){
+                        start++;
+                    }
+
+                    while (start < end && nums[end] == nums[end + 1]){
+                        end--;
+                    }
                 }
             }
         }
-        set<vector<int>> s(a.begin(), a.end());
-        vector<vector<int>> k(s.begin(), s.end());
-        return k;
+        return a;
     }
 };
